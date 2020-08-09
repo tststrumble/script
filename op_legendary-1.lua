f8e0637962ac06e1a48021749d410f3ab0a1ed34
tststrumble <51537499+tststrumble@users.noreply.github.com>
2020-08-09 23:28:07 +0300

    Create op_legendary-1.lua

---
+++ op_legendary-1.lua
@@ -0,0 +1,99 @@
+while wait() do
+    local t = {}
+    local plr = game:service'Players'.LocalPlayer
+    for i,v in pairs(workspace:GetChildren()) do 
+        spawn(function()
+            if v.Name == "Ope Fruit" then
+            v.Handle.CFrame = plr.Character.HumanoidRootPart.CFrame
+            end
+            if v.Name == "Smelt Fruit" then
+            v.Handle.CFrame = plr.Character.HumanoidRootPart.CFrame
+            end
+            if v.Name == "Magma Fruit" then
+            v.Handle.CFrame = plr.Character.HumanoidRootPart.CFrame
+            end
+            if v.Name == "Gas Fruit" then
+            v.Handle.CFrame = plr.Character.HumanoidRootPart.CFrame
+            end
+            if v.Name == "Sand Fruit" then
+            v.Handle.CFrame = plr.Character.HumanoidRootPart.CFrame
+            end
+            if v.Name == "Dark Fruit" then
+            v.Handle.CFrame = plr.Character.HumanoidRootPart.CFrame
+            end
+            if v.Name == "Chilly Fruit" then
+            v.Handle.CFrame = plr.Character.HumanoidRootPart.CFrame
+            end
+            if v.Name == "Rumble Fruit" then
+            v.Handle.CFrame = plr.Character.HumanoidRootPart.CFrame
+            end
+            if v.Name == "Slip Fruit" then
+            v.Handle.CFrame = plr.Character.HumanoidRootPart.CFrame
+            end
+            if v.Name == "Float Fruit" then
+            v.Handle.CFrame = plr.Character.HumanoidRootPart.CFrame
+            end
+            if v.Name == "Smoke Fruit" then
+            v.Handle.CFrame = plr.Character.HumanoidRootPart.CFrame
+            end
+            if v.Name == "Barrier Fruit" then
+            v.Handle.CFrame = plr.Character.HumanoidRootPart.CFrame
+            end
+            if v.Name == "Gum Fruit" then
+            v.Handle.CFrame = plr.Character.HumanoidRootPart.CFrame
+            end
+            if v.Name == "Hot Fruit" then
+            v.Handle.CFrame = plr.Character.HumanoidRootPart.CFrame
+            end
+            if v.Name == "Clear Fruit" then
+            v.Handle.CFrame = plr.Character.HumanoidRootPart.CFrame
+            end
+            if v.Name == "Diamond Fruit" then
+            v.Handle.CFrame = plr.Character.HumanoidRootPart.CFrame
+            end
+            if v.Name == "Return Fruit" then
+            v.Handle.CFrame = plr.Character.HumanoidRootPart.CFrame
+            end
+            if v.Name == "More Fruit" then
+            v.Handle.CFrame = plr.Character.HumanoidRootPart.CFrame
+            end
+            if v.Name == "Phoenix Fruit" then
+            v.Handle.CFrame = plr.Character.HumanoidRootPart.CFrame
+            end
+            if v.Name == "Chop Fruit" then
+            v.Handle.CFrame = plr.Character.HumanoidRootPart.CFrame
+            end
+            if v.Name == "Spring Fruit" then
+            v.Handle.CFrame = plr.Character.HumanoidRootPart.CFrame
+            end
+            if v.Name == "LightOld Fruit" then
+            v.Handle.CFrame = plr.Character.HumanoidRootPart.CFrame
+            end
+            if v.Name == "Swim Fruit" then
+            v.Handle.CFrame = plr.Character.HumanoidRootPart.CFrame
+            end
+            if v.Name == "Snow Fruit" then
+            v.Handle.CFrame = plr.Character.HumanoidRootPart.CFrame
+            end
+            if v.Name == "Hobby Fruit" then
+            v.Handle.CFrame = plr.Character.HumanoidRootPart.CFrame
+            end
+            if v.Name == "Clone Fruit" then
+            v.Handle.CFrame = plr.Character.HumanoidRootPart.CFrame
+            end
+            if v.Name == "Z_Template Fruit" then
+            v.Handle.CFrame = plr.Character.HumanoidRootPart.CFrame
+            end
+            if v.Name == "String Fruit" then
+            v.Handle.CFrame = plr.Character.HumanoidRootPart.CFrame
+            end
+            if v.Name == "Flare Fruit" then
+            v.Handle.CFrame = plr.Character.HumanoidRootPart.CFrame
+            end
+            if v.Name == "Light Fruit" then
+            v.Handle.CFrame = plr.Character.HumanoidRootPart.CFrame
+            end
+ 
+        end)
+    end
+end
